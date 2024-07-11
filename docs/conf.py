1dbd3fcf2ddd644319694c6fce14cb7ddafb4ecf
hannahbaumann <hbaumann@uci.edu>
2024-07-11 10:15:22 +0200

    add language

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,6 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-language = 'en'
 
 extensions = [
     'sphinx.ext.autosummary',
@@ -75,7 +74,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
