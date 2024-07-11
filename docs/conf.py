709602b6b2088b56979126551f7709f6de570c58
hannahbaumann <hbaumann@uci.edu>
2024-07-11 10:09:36 +0200

    specify language

--- docs/conf.py
+++ docs/conf.py
@@ -41,6 +41,8 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+language = 'en'
+
 extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.autodoc',
