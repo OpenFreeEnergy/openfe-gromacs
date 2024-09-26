e1e86f0662758ba72450b301cc01b4b1808173f2
hannahbaumann <hbaumann@uci.edu>
2024-09-26 14:31:50 +0200

    Add integration test of protein-ligand MD

---
+++ openfe_gromacs/tests/protocols/test_gromacs_md_slow.py
@@ -0,0 +1,38 @@
+# This code is part of OpenFE and is licensed under the MIT license.
+# For details, see https://github.com/OpenFreeEnergy/openfe-gromacs
+
+import gufe
+import pytest
+from openff.units import unit as off_unit
+
+from openfe_gromacs.protocols.gromacs_md.md_methods import GromacsMDProtocol
+
+
+@pytest.mark.integration
+def test_protein_ligand_md(toluene_complex_system, tmp_path_factory):
+    settings = GromacsMDProtocol.default_settings()
+    settings.simulation_settings_em.nsteps = 10
+    settings.simulation_settings_nvt.nsteps = 10
+    settings.simulation_settings_npt.nsteps = 100
+    settings.simulation_settings_em.rcoulomb = 1.0 * off_unit.nanometer
+    settings.simulation_settings_nvt.rcoulomb = 1.0 * off_unit.nanometer
+    settings.simulation_settings_npt.rcoulomb = 1.0 * off_unit.nanometer
+    protocol = GromacsMDProtocol(
+        settings=settings,
+    )
+
+    dag = protocol.create(
+        stateA=toluene_complex_system,
+        stateB=toluene_complex_system,
+        mapping=None,
+    )
+
+    shared_temp = tmp_path_factory.mktemp("shared")
+    scratch_temp = tmp_path_factory.mktemp("scratch")
+    gufe.protocols.execute_DAG(
+        dag,
+        shared_basedir=shared_temp,
+        scratch_basedir=scratch_temp,
+        keep_shared=False,
+        n_retries=3,
+    )
