96861c9fe9e24f073cc37629a5d37f4a5708159b
hannahbaumann <hbaumann@uci.edu>
2024-09-30 14:53:24 +0200

    Make slow test much slower

--- openfe_gromacs/tests/protocols/test_gromacs_md_slow.py
+++ openfe_gromacs/tests/protocols/test_gromacs_md_slow.py
@@ -11,9 +11,9 @@ from openfe_gromacs.protocols.gromacs_md.md_methods import GromacsMDProtocol
 @pytest.mark.integration
 def test_protein_ligand_md(toluene_complex_system, tmp_path_factory):
     settings = GromacsMDProtocol.default_settings()
-    settings.simulation_settings_em.nsteps = 10
-    settings.simulation_settings_nvt.nsteps = 10
-    settings.simulation_settings_npt.nsteps = 10000
+    settings.simulation_settings_em.nsteps = 5000
+    settings.simulation_settings_nvt.nsteps = 10000
+    settings.simulation_settings_npt.nsteps = 100000
     protocol = GromacsMDProtocol(
         settings=settings,
     )
